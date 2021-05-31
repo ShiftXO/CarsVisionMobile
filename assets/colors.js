6cdab304e27f0d0407f8fefe6f8291e274887928
Vladimir Mollov <vladimirmolav@abv.bg>
2021-05-31 22:58:07 +0300

    Create data.

---
+++ assets/colors.js
@@ -0,0 +1,6 @@
+export const blue = '#189ad3';
+export const gray = '#acb0b4';
+export const gray2 = '#e1e5ea';
+export const gray3 = '#9a9a9a';
+export const white = '#ffffff';
+export const backgroundGray = '#e5e5e5';
---
+++ assets/data.js
@@ -0,0 +1,6 @@
+export const options = {
+    engine: ['Petrol', 'Diesel', 'Electric', 'Hybrid'],
+    gearbox: ['Manual', 'Automatic', 'Semi-Automatic'],
+    eurostandart: ['Euro 1', 'Euro 2', 'Euro 3', 'Euro 4', 'Euro 5', 'Euro 6'],
+    color: ['Black', 'White', 'Red', 'Blue', 'Gray'],
+}
